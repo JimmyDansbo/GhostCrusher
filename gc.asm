491cc41e1cb8eb9feef9b3d739f713d73fb36d5f
Jimmy Dansbo <jimmy@dansbo.dk>
2019-12-01 08:39:19 +0100

    Modified splash screen and added screenshot to README Initialization and writing/drawing of splash screen has been moved to a separate function. Initialization of VERA has been changed to a macro and moved to x16.inc file.

--- gc.asm
+++ gc.asm
@@ -8,14 +8,26 @@
 
 
 main:
-	lda	#0
-	sta	VERA_CTRL
-	sta	VERA_ADDR_BANK
+	jsr	splash_screen
 
-	lda	#$05
+	+vera_init
+
+	jmp	*
+	rts
+
+; **************************************************************************
+; Ensure that the program is running in 80 column mode, set black
+; background, clear the screen and write/draw the splash screen.
+; **************************************************************************
+splash_screen:
+	lda	#2			; Ensure 80x60 mode
+	sec
+	jsr	SCRMOD
+
+	lda	#WALL_COL		; Set black background
 	sta	COLOR_PORT
 
-	lda	#147
+	lda	#147			; Clear Screen
 	jsr	CHROUT
 
 	+gotoxy 2, 10
@@ -38,136 +50,74 @@ main:
 	ldx	#<.gc5
 	ldy	#>.gc5
 	jsr	print_str
-
 	+gotoxy 23, 18
 	ldx	#<.name
 	ldy	#>.name
 	jsr	print_str
-
 	+gotoxy	27, 20
 	ldx	#<.mail
 	ldy	#>.mail
 	jsr	print_str
-
 	+gotoxy	27, 27
 	ldx	#<.forcx16
 	ldy	#>.forcx16
 	jsr	print_str
-
 	+gotoxy 24, 36
 	ldx	#<.pstart
 	ldy	#>.pstart
 	jsr	print_str
-
-	lda	#$04
-	sta	COLOR_PORT
 	+gotoxy	38, 24
 	ldx	#<.xl1
 	ldy	#>.xl1
 	jsr	print_str
-	lda	#$0E
-	sta	COLOR_PORT
 	+gotoxy 39, 25
 	ldx	#<.xl2
 	ldy	#>.xl2
 	jsr	print_str
-	lda	#$03
-	sta	COLOR_PORT
 	+gotoxy 40, 26
 	ldx	#<.xl3
 	ldy	#>.xl3
 	jsr	print_str
-	lda	#$07
-	sta	COLOR_PORT
 	+gotoxy 40, 28
 	ldx	#<.xl5
 	ldy	#>.xl5
 	jsr	print_str
-	lda	#$08
-	sta	COLOR_PORT
 	+gotoxy 39, 29
 	ldx	#<.xl6
 	ldy	#>.xl6
 	jsr	print_str
-	lda	#$02
-	sta	COLOR_PORT
 	+gotoxy 38, 30
 	ldx	#<.xl7
 	ldy	#>.xl7
 	jsr	print_str
-
-	+vera_goxy 2, 11	; In G
-	+vera_chrout SWALL
-	+vera_color SWALL_COL
-
-	+vera_goxy 9, 11	; Player char in H
-	+vera_chrout PLAYER
-	+vera_color PLAY_COL
-
-	+vera_goxy 14, 13	; In O
-	+vera_chrout SWALL
-	+vera_color SWALL_COL
-
-	+vera_goxy 17, 12	; Ghost in O
-	+vera_chrout GHOST
-	+vera_color GHOST_COL
-
-	+vera_goxy 22, 11	; Portal in S
-	+vera_chrout PORTAL
-	+vera_color GHOST_COL
-
-	+vera_goxy 27, 10	; In T
-	+vera_chrout SWALL
-	+vera_color SWALL_COL
-
-	+vera_goxy 35, 11	; Ghost in C
-	+vera_chrout GHOST
-	+vera_color GHOST_COL
-
-	+vera_goxy 41, 12	; In R
-	+vera_chrout SWALL
-	+vera_color SWALL_COL
-
-	+vera_goxy 41, 13	; Super Ghost in R
-	+vera_chrout SGHOST
-	+vera_color GHOST_COL
-
-	+vera_goxy 47, 13	; Ghost in U
-	+vera_chrout GHOST
-	+vera_color GHOST_COL
-
-	+vera_goxy 49, 14	; In U
-	+vera_chrout SWALL
-	+vera_color SWALL_COL
-
-	+vera_goxy 52, 14	; In S
-	+vera_chrout SWALL
-	+vera_color SWALL_COL
-
-	+vera_goxy 53, 11	; Portal in S
-	+vera_chrout PORTAL
-	+vera_color GHOST_COL
-
-	+vera_goxy 58, 13	; In H
-	+vera_chrout SWALL
-	+vera_color SWALL_COL
-	+vera_chrout HGHOST	; Hatched Ghost in H
-	+vera_color GHOST_COL
-
-
-	+vera_goxy 64, 10	; In E
-	+vera_chrout SWALL
-	+vera_color SWALL_COL
-
-	+vera_goxy 67, 10	; In E
-	+vera_chrout SWALL
-	+vera_color SWALL_COL
-
-	+vera_goxy 75, 11	; In R
-	+vera_chrout SWALL
-	+vera_color SWALL_COL
-
-	jmp	*
+	+gotoxy 35, 40
+	ldx	#<.player_text
+	ldy	#>.player_text
+	jsr	print_str
+	+gotoxy 35, 42
+	ldx	#<.portal_text
+	ldy	#>.portal_text
+	jsr	print_str
+	+gotoxy 35, 44
+	ldx	#<.ghost_text
+	ldy	#>.ghost_text
+	jsr	print_str
+	+gotoxy 35, 46
+	ldx	#<.pghost_text
+	ldy	#>.pghost_text
+	jsr	print_str
+	+gotoxy 35, 48
+	ldx	#<.dghost_text
+	ldy	#>.dghost_text
+	jsr	print_str
+	+gotoxy 35, 50
+	ldx	#<.wall_text
+	ldy	#>.wall_text
+	jsr	print_str
+	+gotoxy 35, 52
+	ldx	#<.swall_text
+	ldy	#>.swall_text
+	jsr	print_str
 	rts
 
 print_str:
