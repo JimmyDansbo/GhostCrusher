dfedc3fea3b190f6cabfb3065290da582ae02de0
Jimmy Dansbo <jimmy@dansbo.dk>
2019-12-08 23:03:37 +0100

    Function check if player can move in chosen direction Created the ability to poke a seed into the random number generator to create the same game each time. Created a function that check if it is possible for the player to move in the chosen direction. Next up is the function to actually to the move. It is going to be very similar to the one that checks to move.

--- gc.asm
+++ gc.asm
@@ -1,18 +1,25 @@
 *=$0801
-!byte $0C,$08,$0A,$00,$9E,$20,$32,$30,$36,$34,$00,$00,$00
+!byte $0C,$08,$0A,$00,$9E,' ','2','0','6','4',$00,$00,$00
 *=$0810
 
 RANDNUM		=	$00		; 2 bytes for RANDOM seed
-IRQ_TRIG	=	RANDNUM+2	; ZP address to show if IRQ triggered
-LEVEL		=	IRQ_TRIG+1	; ZP address to store current level
-LIVES		=	LEVEL+1		; ZP address to store number of lives
-NUMGHOSTS	=	LIVES+1		; Number of 'normal' ghosts
-NUMPGHOSTS	=	NUMGHOSTS+1	; Number of polter geists
-NUMDGHOSTS	=	NUMPGHOSTS+1	; Number of dimentional ghosts
-NUMPORTALS	=	NUMDGHOSTS+1	; Number of portals
-POINTS		=	NUMPORTALS+1	; 3 bytes
-JIFFIES		=	POINTS+3
-PLAYER_DELAY	=	JIFFIES+1
+IRQ_TRIG	=	$02		; ZP address to show if IRQ triggered
+LEVEL		=	$03		; ZP address to store current level
+LIVES		=	$04		; ZP address to store number of lives
+NUMGHOSTS	=	$05		; Number of 'normal' ghosts
+NUMPGHOSTS	=	$06		; Number of polter geists
+NUMDGHOSTS	=	$07		; Number of dimentional ghosts
+NUMPORTALS	=	$08		; Number of portals
+POINTS		=	$09		; 3 bytes
+BTN_UP		=	$0C
+BTN_DN		=	$0D
+BTN_LT		=	$0E
+BTN_RT		=	$0F
+JIFFIES		=	$10
+PLAYER_DELAY	=	$11
+PLAYER_X	=	$12
+PLAYER_Y	=	$13
+RANDSEED	=	$18		; 2 bytes
 
 	jmp	main
 
@@ -83,6 +90,18 @@ main:
 	and	#NES_STA
 	bne	.start_wait
 
+	ldx	#0		; If there is 0 in RANDSEED
+	cpx	RANDSEED	; We save RANDNUM in RANDSEED
+	bne	+		; Otherwise we copy RANDSEED to RANDNUM
+	ldx	RANDNUM
+	stx	RANDSEED
+	ldx	RANDNUM+1
+	stx	RANDSEED+1
++	ldx	RANDSEED
+	stx	RANDNUM
+	ldx	RANDSEED+1
+	stx	RANDNUM+1
+
 	jsr	draw_border
 	ldy	#12
 	jsr	place_swalls
@@ -103,12 +122,222 @@ game_loop:
 
 do_game:
 	jsr	do_clock
+	jsr	do_getjoy
 	jsr	do_player
 	rts
 
 ; *******************************************************************
 ; *******************************************************************
+do_getjoy:
+	jsr	GETJOY
+	ldy	#1
+	lda	JOY1
+	and	#JOY_DN
+	bne	.check_lt
+	sty	BTN_DN
+.check_lt:
+	lda	JOY1
+	and	#JOY_LT
+	bne	.check_rt
+	sty	BTN_LT
+.check_rt:
+	lda	JOY1
+	and	#JOY_RT
+	bne	.check_up
+	sty	BTN_RT
+.check_up:
+	lda	JOY1
+	and	#JOY_UP
+	bne	.dgj_end
+	sty	BTN_UP
+.dgj_end
+	rts
+
+; *******************************************************************
+; *******************************************************************
+can_move:
+	sta	TMP2
+	lda	#0
+	sta	TMP3		; Hold previous field
+-	lda	TMP2
+	cmp	#1
+	bne	.is_lt
+	inc	TMP1		; INC Y
+	jmp	.do_find
+.is_lt:
+	cmp	#2
+	bne	.is_rt
+	dec	TMP0		; DEC X
+	jmp	.do_find
+.is_rt:
+	cmp	#3
+	bne	.is_up
+	inc	TMP0		; INC X
+	jmp	.do_find
+.is_up:
+	dec	TMP1		; DEC Y
+
+.do_find:
+	lda	TMP1		; Write Y to VERA
+	sta	VERA_ADDR_HIGH
+	lda	TMP0		; Load X
+	asl
+	sta	VERA_ADDR_LOW	; Write X to VERA
+	lda	VERA_DATA0	; Load char at address
+	cmp	#' '
+	bne	.is_ghost
+	; zero flag is already set
+	rts
+.is_ghost:
+	cmp	#GHOST		; If this is not ghost, check next
+	bne	.is_pghost
+	lda	TMP3		; Load previous field
+	cmp	#WALL		; If it is a WALL field, we need to
+	beq	+		; see what next field is
+	lda	#0		; Else return that we can move
+	rts			; (allthough it will kill the player)
++	ldy	#GHOST		; Save the current field
+	sty	TMP3
+	jmp	-		; Check next field
+
+.is_pghost:
+	cmp	#PGHOST		; If this is not pghost, check next
+	bne	.is_dghost
+	lda	TMP3		; Load previous field
+	cmp	#WALL		; If it is a WALL field, we need to
+	beq	+		; see what next field is
+	lda	#0		; Else return that we can move
+	rts
++	ldy	#PGHOST		; Save the current field
+	sty	TMP3
+	jmp	-		; Check next field
+.is_dghost:
+	cmp	#DGHOST		; If this is not dghost, check next
+	bne	.is_portal
+	lda	TMP3		; Load previous field
+	cmp	#WALL		; If it is a WALL field, we need to
+	beq	+		; see what next field is
+	lda	#0		; Else return that we can move
+	rts
++	ldy	#DGHOST		; Save the current field
+	sty	TMP3
+	jmp	-		; Check next field
+.is_portal:
+	cmp	#PORTAL		; If this is not portal, check next
+	bne	.is_wall
+	lda	TMP3		; Load previous field
+	cmp	#WALL		; If it is a WALL field, we need to
+	beq	+		; see what next field is
+	lda	#0		; Else return that we can move
+	rts
++	ldy	#PORTAL		; Save the current field
+	sty	TMP3
+	jmp	-		; Check next field
+.is_wall:
+	cmp	#WALL		; If this is not wall, check next
+	bne	.is_swall
+	lda	TMP3		; Load previous field
+	cmp	#GHOST		; If previous is not ghost, check next
+	bne	+
+	lda	#0		; Set zero flag and return (we can move)
+	rts
++	cmp	#PORTAL		; If previous is not portal, check next
+	bne	+
+	lda	#0		; Set zero flag and return (we can move)
+	rts
++	cmp	#PGHOST		; If previous is not pghost, check next
+	bne	+
+	lda	#1		; Reset zero and return (we can not move)
+	rts
++	cmp	#DGHOST		; If previous is not dghost, check next
+	bne	+
+	lda	#1		; Reset zero and return (we can not move)
+	rts
++	ldy	#WALL		; Save the current field
+	sty	TMP3
+	jmp	-		; Check next field
+.is_swall:
+	lda	TMP3		; Load previous field
+	cmp	#GHOST		; If previous was not ghost, check next
+	bne	+
+	lda	#0		; Set zero and return (we can move)
+	rts
++	cmp	#PORTAL		; If previous was not portal, check next
+	bne	+
+	lda	#0		; Set zero and return (we can move)
+	rts
++	cmp	#DGHOST		; If previous was not dghost, check next
+	bne	+
+	lda	#0		; Set zero and return (we can move)
+	rts
++	cmp	#PGHOST		; If previous was not pghost, check next
+	bne	+
+	lda	#0		; Set zero and return (we can move)
+	rts
++	lda	#1		; Reset zero and return (we can not move)
+	rts
+
+; *******************************************************************
+; *******************************************************************
+do_move:
+; Handle moving
+	rts
+
+; *******************************************************************
+; *******************************************************************
 do_player:
+	dec	PLAYER_DELAY
+	beq	.do_player
+	rts
+.do_player:
+	lda	#20
+	sta	PLAYER_DELAY
+
+	lda	PLAYER_X
+	sta	TMP0
+	lda	PLAYER_Y
+	sta	TMP1
+
+	lda	BTN_DN
+	beq	.btn_lt
+	; Handle down button
+	lda	#1
+	jsr	can_move
+	bne	.dp_end
+	lda	#1
+	jsr	do_move
+	jmp	.dp_end
+.btn_lt:
+	lda	BTN_LT
+	beq	.btn_rt
+	; Handle left button
+	lda	#2
+	jsr	can_move
+	bne	.dp_end
+	lda	#2
+	jsr	do_move
+	jmp	.dp_end
+.btn_rt:
+	lda	BTN_RT
+	beq	.btn_up
+	; Handle right button
+	lda	#3
+	jsr	can_move
+	bne	.dp_end
+	lda	#3
+	jsr	do_move
+	jmp	.dp_end
+.btn_up:
+	lda	BTN_UP
+	beq	.dp_end
+	; Handle up button
+	lda	#4
+	jsr	can_move
+	bne	.dp_end
+	lda	#4
+	jsr	do_move
+
+.dp_end:
 	rts
 
 ; *******************************************************************
@@ -192,6 +421,8 @@ do_clock:
 ; *******************************************************************
 place_player:
 	jsr	find_empty
+	sty	PLAYER_Y
+	stx	PLAYER_X
 	lda	#PLAYER
 	sta	VERA_DATA0
 	inc	VERA_ADDR_LOW
@@ -205,10 +436,14 @@ place_player:
 ; *******************************************************************
 ; USES:		A
 ; *******************************************************************
+; RETURNS:	X and Y as the coordinates found
+; *******************************************************************
 find_empty:
 	+rand	3, 59
 	sta	VERA_ADDR_HIGH
+	tay
 	+rand	1, 79
+	tax
 	asl
 	sta	VERA_ADDR_LOW
 	lda	VERA_DATA0
@@ -227,6 +462,7 @@ place_ghosts:
 	ldy	#0
 	sty	VERA_ADDR_BANK		; No increment
 	ldy	NUMGHOSTS
+	sty	TMP0
 .doghosts:
 	beq	.startpghosts
 	; Handle ghosts
@@ -236,10 +472,11 @@ place_ghosts:
 	inc	VERA_ADDR_LOW
 	lda	#GHOST_COL		; Write ghost color
 	sta	VERA_DATA0
-	dey
+	dec	TMP0
 	jmp	.doghosts
 .startpghosts:
 	ldy	NUMPGHOSTS
+	sty	TMP0
 .dopghosts:
 	beq	.startportals
 	; Handle poltergeists
@@ -249,10 +486,11 @@ place_ghosts:
 	inc	VERA_ADDR_LOW
 	lda	#GHOST_COL		; Write ghost color
 	sta	VERA_DATA0
-	dey
+	dec	TMP0
 	jmp	.dopghosts
 .startportals:
 	ldy	NUMPORTALS
+	sty	TMP0
 .doportals:
 	beq	.pg_end
 	; Handle portals
@@ -262,7 +500,7 @@ place_ghosts:
 	inc	VERA_ADDR_LOW
 	lda	#GHOST_COL		; Write ghost color
 	sta	VERA_DATA0
-	dey
+	dec	TMP0
 	jmp	.doportals
 .pg_end:
 	rts
